4b2a36e83685d52bec9ed051105fc747dd3b58e8
JuanHenrique319 <80597368+JuanHenrique319@users.noreply.github.com>
2021-05-14 19:37:50 -0300

    Add files via upload

---
+++ code.js
@@ -0,0 +1,12 @@
+
+let ano;
+let nome;
+let idade
+
+nome = prompt ('digite seu nome');
+ano = prompt ('digite seu ano de nascimento');
+
+idade = 2021 - ano;
+alert (`olá seu nome é ${nome}, tens exatamente ${idade} anos.`)
+
+console.log(frase);
